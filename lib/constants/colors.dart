4fb989dd744bdac637f8095122c197fc75a7da6c
arun-kumar-bidila <arunkumarbidila@gmail.com>
2025-02-28 23:54:36 +0530

    feat(myexams):completed upcoming and live pages

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 class GlobalColors {
-   
-
-  
   static const backgroundColor = Color(0xFFF2F2F2);
-  static const buttonColor =Color(0xFFCA174D);
-  static const inactivebuttonColor=Colors.white;
-  static const textBlack=Colors.black;
-  static const textWhite=Colors.white;
-  static const viewall=Color(0xFFEF7F1A);
+  static const buttonColor = Color(0xFFCA174D);
+  static const inactivebuttonColor = Colors.white;
+  static const textBlack = Colors.black;
+  static const textWhite = Colors.white;
+  static const viewall = Color(0xFFEF7F1A);
+  static const liveExamText = Color(0xFF5D5D5D);
 }
