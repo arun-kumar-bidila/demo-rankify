62cee91ddd5fb6c304bc5fcba4a2698e5fa5bead
arun-kumar-bidila <arunkumarbidila@gmail.com>
2025-02-28 13:52:41 +0530

    feat(viewall):upcoming exams viewall feature

--- lib/features/dashboard/screens/examtabs/screens/others.dart
+++ lib/features/dashboard/screens/examtabs/screens/others.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:rankify/features/dashboard/screens/examtabs/widgets/toprankers.dart';
+import 'package:rankify/features/dashboard/screens/examtabs/widgets/upcomingexams.dart';
 
 class Others extends StatelessWidget {
   const Others({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "This is Others tab"
-      ),
+     return Column(
+      children: [
+        //toprankers
+        Toprankers(),
+          
+        //upcoming exams
+        Upcomingexams()
+      ],
     );
   }
 }
